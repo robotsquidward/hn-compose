e813390289303fda52d3e5116db137152fe55f84
AJ Kueterman <ajkueterman@gmail.com>
2020-04-27 08:24:33 -0400

    Add basic vertical scroller

--- app/src/main/java/com/example/hncompose/MainActivity.kt
+++ app/src/main/java/com/example/hncompose/MainActivity.kt
@@ -3,17 +3,48 @@ package com.example.hncompose
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.remember
+import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Text
+import androidx.ui.foundation.VerticalScroller
+import androidx.ui.layout.Column
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.Scaffold
+import androidx.ui.material.ScaffoldState
 import androidx.ui.tooling.preview.Preview
+
+val stories: List<String> = listOf("Story One", "Story Two", "Story Three")
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Greeting("Android")
+                TopNewsScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun TopNewsScreen(scaffoldState: ScaffoldState = remember { ScaffoldState() }) {
+    Scaffold(
+            scaffoldState = scaffoldState,
+            bodyContent = { modifier ->
+                TopNewsScreenBody(posts = stories)
+            }
+    )
+}
+
+@Composable
+fun TopNewsScreenBody(
+        posts: List<String>,
+        modifier: Modifier = Modifier) {
+    VerticalScroller {
+        Column {
+            posts.forEach { post ->
+                Greeting(name = post)
             }
         }
     }
@@ -28,6 +59,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MaterialTheme {
-        Greeting("World")
+        TopNewsScreen()
     }
 }
