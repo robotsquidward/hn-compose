589953b4b01c45c53fe8f155927c585102aa46e0
AJ Kueterman <ajkueterman@gmail.com>
2020-07-07 10:10:25 -0400

    Gut the UI screens so we have a good starting point.

--- start/app/src/main/java/com/example/hncompose/MainActivity.kt
+++ start/app/src/main/java/com/example/hncompose/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.ui.material.Scaffold
 import androidx.ui.material.ScaffoldState
 import androidx.ui.material.TopAppBar
 import androidx.ui.res.vectorResource
+import androidx.ui.tooling.preview.Preview
 import com.example.hackernetwork.HackerNewsRepo
 import com.example.hackernetwork.HackerNewsRetrofit
 import com.example.hncompose.data.AppDataStatus
@@ -25,73 +26,21 @@ import com.example.hncompose.viewmodel.HackerNewsViewModel
 import com.example.util.createWithFactory
 
 class MainActivity : AppCompatActivity() {
-
-    private val hackerNewsViewModel: HackerNewsViewModel by viewModels {
-        createWithFactory {
-            HackerNewsViewModel(
-                repo = HackerNewsRepo(
-                    HackerNewsRetrofit.retrofitInstance
-                )
-            )
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppContent(hackerNewsViewModel.listenerHandler)
-        }
-        hackerNewsViewModel.getTopStories()
-    }
-
-    override fun onBackPressed() {
-        if (AppScreenStatus.currentScreen == Screen.Favorites) {
-            AppScreenStatus.currentScreen = Screen.TopNews
+            AppContent()
         }
     }
 }
 
 @Composable
-fun AppContent(listenerHandler: HackerNewsViewModel.HackerNewsListenerHandler) {
-    JetnewsTheme {
-        Scaffold(
-            scaffoldState = ScaffoldState(),
-            topAppBar = {
-                TopAppBar(
-                    title = { Text(text = AppScreenStatus.currentScreen.title) },
-                    actions = {
-                        when (AppScreenStatus.currentScreen) {
-                            Screen.TopNews -> {
-                                IconButton(onClick = {
-                                    AppScreenStatus.currentScreen = Screen.Favorites
-                                }) {
-                                    Icon(asset = vectorResource(id = R.drawable.ic_baseline_star_24))
-                                }
-                            }
-                            Screen.Favorites -> {
-                                IconButton(onClick = {
-                                    AppScreenStatus.currentScreen = Screen.TopNews
-                                }) {
-                                    Icon(asset = vectorResource(id = R.drawable.ic_baseline_home_24))
-                                }
-                            }
-                        }
-                    }
-                )
-            },
-            bodyContent = {
-                Crossfade(current = AppScreenStatus.currentScreen) { screen ->
-                    when (screen) {
-                        is Screen.TopNews -> TopNewsScreen(
-                            AppDataStatus,
-                            listenerHandler.handleLoadMoreTopStories
-                        )
-                        is Screen.Favorites -> FavoritesScreen(
-                            AppDataStatus
-                        )
-                    }
-                }
-            }
-        )
-    }
+fun AppContent() {
+    Text("Hello World!")
+}
+
+@Preview
+@Composable
+fun PreviewMain() {
+    AppContent()
 }
