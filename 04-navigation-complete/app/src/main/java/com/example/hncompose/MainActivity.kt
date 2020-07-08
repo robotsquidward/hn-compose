b3e7cd151e322e0b17db006ccdde6acfb1ea91d2
AJ Kueterman <ajkueterman@gmail.com>
2020-07-08 13:21:03 -0400

    Enable navigation in the TopAppBar

--- 04-navigation-complete/app/src/main/java/com/example/hncompose/MainActivity.kt
+++ 04-navigation-complete/app/src/main/java/com/example/hncompose/MainActivity.kt
@@ -1,52 +1,76 @@
 package com.example.hncompose
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.getValue
 import androidx.compose.state
 import androidx.ui.animation.Crossfade
 import androidx.ui.core.setContent
-import androidx.ui.foundation.Icon
+import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
 import androidx.ui.material.IconButton
 import androidx.ui.material.Scaffold
-import androidx.ui.material.ScaffoldState
 import androidx.ui.material.TopAppBar
 import androidx.ui.res.vectorResource
 import androidx.ui.tooling.preview.Preview
-import com.example.hackernetwork.HackerNewsRepo
-import com.example.hackernetwork.HackerNewsRetrofit
 import com.example.hncompose.data.AppDataStatus
 import com.example.hncompose.data.AppScreenStatus
 import com.example.hncompose.extension.mock
 import com.example.hncompose.model.Screen
-import com.example.hncompose.theme.JetnewsTheme
 import com.example.hncompose.ui.FavoritesScreen
 import com.example.hncompose.ui.TopNewsScreen
-import com.example.hncompose.viewmodel.HackerNewsViewModel
-import com.example.util.createWithFactory
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AppContent()
         }
     }
+
 }
 
 @Composable
 fun AppContent() {
+    val screenStatus by state { AppScreenStatus }
+
     Scaffold(
         topAppBar = {
-            TopAppBar(title = {
-                Text("Hacker Compose")
-            })
+            TopAppBar(
+                title = { 
+                    Text("Hacker Compose") 
+                },
+                actions = {
+                    Crossfade(current = screenStatus.currentScreen) { state ->
+                        when(state) {
+                            Screen.TopNews -> {
+                                IconButton(onClick = {
+                                    screenStatus.currentScreen = Screen.Favorites
+                                }) {
+                                    Image(asset = vectorResource(id = R.drawable.ic_baseline_star_24))
+                                }
+                            }
+                            Screen.Favorites -> {
+                                IconButton(onClick = {
+                                    screenStatus.currentScreen = Screen.TopNews
+                                }) {
+                                    Image(asset = vectorResource(id = R.drawable.ic_baseline_home_24))
+                                }
+                            }
+                        }
+                    }
+                }
+            )
         }
     ) {
-        TopNewsScreen(appData = AppDataStatus.mock())
+        Crossfade(current = screenStatus.currentScreen) { state ->
+            when (state) {
+                Screen.TopNews -> TopNewsScreen(appData = AppDataStatus.mock())
+                Screen.Favorites -> FavoritesScreen()
+            }
+        }
     }
 }
 
