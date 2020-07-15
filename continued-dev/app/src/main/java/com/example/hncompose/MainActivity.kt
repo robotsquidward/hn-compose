a6880eb8dcd20ba8bcade5dd72ce2006191fcca2
AJ Kueterman <ajkueterman@gmail.com>
2020-07-15 10:59:11 -0400

    Create a new directory for future development of HN Compose

---
+++ continued-dev/app/src/main/java/com/example/hncompose/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.hncompose
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.animation.Crossfade
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Image
+import androidx.ui.foundation.Text
+import androidx.ui.material.IconButton
+import androidx.ui.material.Scaffold
+import androidx.ui.material.TopAppBar
+import androidx.ui.res.vectorResource
+import androidx.ui.tooling.preview.Preview
+import com.example.hackernetwork.HackerNewsRepo
+import com.example.hackernetwork.HackerNewsRetrofit
+import com.example.hncompose.data.AppDataStatus
+import com.example.hncompose.data.AppScreenStatus
+import com.example.hncompose.model.Screen
+import com.example.hncompose.theme.HackerNewsTheme
+import com.example.hncompose.ui.FavoritesScreen
+import com.example.hncompose.ui.TopNewsScreen
+import com.example.hncompose.viewmodel.HackerNewsViewModel
+import com.example.util.createWithFactory
+
+class MainActivity : AppCompatActivity() {
+
+    private val hackerNewsViewModel: HackerNewsViewModel by viewModels {
+        createWithFactory {
+            HackerNewsViewModel(
+                repo = HackerNewsRepo(
+                    HackerNewsRetrofit.retrofitInstance
+                )
+            )
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HackerNewsTheme {
+                AppContent(hackerNewsViewModel.listenerHandler.handleLoadMoreTopStories)
+            }
+        }
+
+        hackerNewsViewModel.getTopStories()
+    }
+
+}
+
+@Composable
+fun AppContent(loadMoreStoriesClicked: () -> Unit) {
+    Scaffold(
+        topAppBar = {
+            TopAppBar(
+                title = { 
+                    Text("Hacker Compose") 
+                },
+                actions = {
+                    Crossfade(current = AppScreenStatus.currentScreen) { state ->
+                        when(state) {
+                            Screen.TopNews -> {
+                                IconButton(onClick = {
+                                    AppScreenStatus.currentScreen = Screen.Favorites
+                                }) {
+                                    Image(asset = vectorResource(id = R.drawable.ic_baseline_star_24))
+                                }
+                            }
+                            Screen.Favorites -> {
+                                IconButton(onClick = {
+                                    AppScreenStatus.currentScreen = Screen.TopNews
+                                }) {
+                                    Image(asset = vectorResource(id = R.drawable.ic_baseline_home_24))
+                                }
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    ) {
+        Crossfade(current = AppScreenStatus.currentScreen) { state ->
+            when (state) {
+                Screen.TopNews -> TopNewsScreen(appData = AppDataStatus, loadMoreStoriesClicked = loadMoreStoriesClicked)
+                Screen.Favorites -> FavoritesScreen(appData = AppDataStatus)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMain() {
+    HackerNewsTheme {
+        AppContent {}
+    }
+}
---
+++ continued-dev/app/src/main/java/com/example/hncompose/ui/FavoritesScreen.kt
@@ -0,0 +1,68 @@
+package com.example.hncompose.ui
+
+import androidx.compose.Composable
+import androidx.ui.core.ContextAmbient
+import androidx.ui.core.Modifier
+import androidx.ui.foundation.*
+import androidx.ui.layout.*
+import androidx.ui.material.*
+import androidx.ui.material.ripple.ripple
+import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
+import com.example.hackernetwork.HNItem
+import com.example.hncompose.data.AppDataStatus
+import com.example.hncompose.extension.mock
+import com.example.hncompose.theme.HackerNewsTheme
+import com.example.util.shortUrlString
+import com.example.util.storyClicked
+
+@Composable
+fun FavoritesScreen(appData: AppDataStatus) {
+    val context = ContextAmbient.current
+    VerticalScroller {
+        Column {
+            for (story in appData.topStories.filter { it.favorite }) {
+                FavoriteCard(favorite = story) {
+                    storyClicked(url = story.url, context = context)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FavoriteCard(favorite: HNItem, favoriteClicked: () -> Unit) {
+    Card(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth()) {
+
+        Clickable(
+            onClick = favoriteClicked,
+            modifier = Modifier.ripple()
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = favorite.title ?: "",
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = favorite.url.shortUrlString,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun FavoritesPreview() {
+    HackerNewsTheme {
+        FavoritesScreen(appData = AppDataStatus.mock())
+    }
+}
